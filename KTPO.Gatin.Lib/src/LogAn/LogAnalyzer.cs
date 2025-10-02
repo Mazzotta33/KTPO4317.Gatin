4a7b79311c93d17ddf8980c6b9b68006cc14116d
Разиль <razilchick@mail.ru>
2025-10-02 13:00:28 +0300

    lab1

---
+++ KTPO.Gatin.Lib/src/LogAn/LogAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿namespace KTPO.Gatin.Lib.LogAn;
+
+public class LogAnalyzer
+{
+    public bool WasLastFileNameValid { get; set; }
+    public bool IsValidLogFileName(string fileName)
+    {
+        WasLastFileNameValid = false;
+        
+        if (string.IsNullOrEmpty(fileName))
+        {
+            throw new ArgumentNullException("Имя файла должно быть задано");
+        }
+        
+        if (!fileName.EndsWith(".Gatin", StringComparison.CurrentCultureIgnoreCase))
+        {
+            return false;
+        }
+        WasLastFileNameValid = true;
+        
+        return true;
+    }
+}
